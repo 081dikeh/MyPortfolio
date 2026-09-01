f949f1266933040d07f9ecc9e6d6084c28bc191d
Dikeh Daniel <dikehdaniel2020@gmail.com>
2026-08-31 23:17:47 -0800

    update: import new resume for download

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -89,7 +89,7 @@ export default function Nav() {
             </ul>
 
             <a
-              href='/Dikeh-Daniel_Frontend_Developer_Resume'
+              href='/Dikeh_Daniel_Frontend_Developer_Resume (2).pdf'
               download
               className='text-xs font-medium px-4 py-2 ring-1 ring-accent text-white hover:bg-accent transition-all duration-300 btn-lift whitespace-nowrap'
             >
